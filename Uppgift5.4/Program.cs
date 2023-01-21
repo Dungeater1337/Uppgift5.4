d94407e127353abdd5e41ea35181bdc10bc04a1a
Dungeater1337 <greg.fredriksson@gmail.com>
2023-01-21 19:52:56 +0100

    Add project files.

---
+++ Uppgift5.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Axel", "Edvin", "Gud", "Lucifer", "Ceasar" };
+            for(int i = 0;i < namn.Length; i++)
+            {
+                Array.Sort(namn);
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
+
